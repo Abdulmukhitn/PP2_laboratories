aa73c64d01f624692311a2406721ec16e0e40e01
Abdulmukhitx <a_nurkazy@kbtu.kz>
2025-02-12 12:18:13 +0500

    generator

--- lab4/generators/again.py
+++ lab4/generators/again.py
@@ -1,1 +1,28 @@
-print("  fk")
+#1
+
+n = int(input())
+some = (x*x for x in range(n))
+for num in some :
+    print(num)
+
+
+#2
+def even(n):
+    for num in range(n):
+        if num % 2 == 0:
+            yield num
+
+for num in even(n):
+    print(num)
+
+#3
+def div(n):
+    for num in range(n):
+        if num % 3 == 0:
+            yield num
+        if num % 4 == 0:
+            yield num
+
+for num in div(n):
+    print(num)
+
